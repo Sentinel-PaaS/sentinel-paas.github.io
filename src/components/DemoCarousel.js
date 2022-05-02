ff1db321fa8f4c52cc6b0145cb7194b2584fb85d
samlipari <70296400+samlipari@users.noreply.github.com>
2022-05-02 18:46:33 -0400

    Adjusted demo carousel css, changed favicon
    Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
    Co-authored-by: Drew Sessler <drew.sessler@gmail.com>
    Co-authored-by: Brendan Leal <leal.brendan@gmail.com>

--- src/components/DemoCarousel.js
+++ src/components/DemoCarousel.js
@@ -4,11 +4,11 @@ function DemoCarousel() {
     return (
         <div
             id="carouselDarkVariant"
-            className="carousel slide carousel-fade carousel-dark relative"
+            className="carousel slide carousel-dark relative"
             data-bs-ride="carousel"
             data-bs-interval="false"
         >
-            <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+            <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 my-4">
                 <button
                     data-bs-target="#carouselDarkVariant"
                     data-bs-slide-to="0"
@@ -35,7 +35,7 @@ function DemoCarousel() {
                         className="block w-full"
                         alt="Motorbike Smoke"
                     />
-                    <div className="carousel-caption hidden md:block absolute text-center">
+                    <div className="carousel-caption hidden md:block absolute text-center mb-4">
                         <h5 className="text-xl">First slide label</h5>
                         <p>Some representative placeholder content for the first slide.</p>
                     </div>
@@ -47,7 +47,7 @@ function DemoCarousel() {
                         className="block w-full"
                         alt="Mountaintop"
                     />
-                    <div className="carousel-caption hidden md:block absolute text-center">
+                    <div className="carousel-caption hidden md:block absolute text-center mb-4">
                         <h5 className="text-xl">Second slide label</h5>
                         <p>Some representative placeholder content for the second slide.</p>
                     </div>
@@ -59,7 +59,7 @@ function DemoCarousel() {
                         className="block w-full"
                         alt="Woman Reading a Book"
                     />
-                    <div className="carousel-caption hidden md:block absolute text-center">
+                    <div className="carousel-caption hidden md:block absolute text-center mb-4">
                         <h5 className="text-xl">Third slide label</h5>
                         <p>Some representative placeholder content for the third slide.</p>
                     </div>
@@ -72,8 +72,8 @@ function DemoCarousel() {
                 data-bs-target="#carouselDarkVariant"
                 data-bs-slide="prev"
             >
-                <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
+                {/* <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span> */}
+                {/* <span className="visually-hidden">Previous</span> */}
             </button>
             <button
                 className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
@@ -81,8 +81,8 @@ function DemoCarousel() {
                 data-bs-target="#carouselDarkVariant"
                 data-bs-slide="next"
             >
-                <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
+                {/* <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span> */}
+                {/* <span className="visually-hidden">Next</span> */}
             </button>
         </div>
     )
